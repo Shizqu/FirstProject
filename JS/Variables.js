e3933d481817dc8729549ad2745e1f9d5bece86b
Shizqu <60578896+Shizqu@users.noreply.github.com>
2021-10-08 09:28:38 +0200

    css

---
+++ JS/Variables.js
@@ -0,0 +1,16 @@
+var message
+console.log("zawartosc message " + message)
+message = "informacja o uzytkowniku"
+console.log("zawartosc message " + message)
+
+/*
+sum =323
+sum += 10
+console.log(sum)
+ */
+
+var tax = 0.3
+sum = 10
+sum += sum * tax
+alert("suma zakupow z podatkiem to " + sum)
+
